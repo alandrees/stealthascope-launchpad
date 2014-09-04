ff3377d726507b23aaec0ce5e49e7b72aeeb6564
Alan Drees <alandrees@theselves.com>
2014-09-04 00:45:06 -0400

    launchpad option object

---
+++ launchpad_options.js
@@ -0,0 +1,9 @@
+var Launchpad = Launchpad || {};
+
+Launchpad.options = { 'devices'      : 1,
+		      'tracks'       : 8,
+		      'scenes'       : 8,
+		      'startupmode'  : true,
+		      'layout_order' :[{'anchor'   : 1,
+					'position' : 'below'}]};
+			  
