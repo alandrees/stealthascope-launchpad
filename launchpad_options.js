a97f0a351cc642239ed3074a1082be815921dfcb
Alan Drees <alandrees@theselves.com>
2015-06-13 05:18:19 -0400

    added a file documentation header for launchpad_options.js

--- launchpad_options.js
+++ launchpad_options.js
@@ -1,3 +1,12 @@
+/**
+ * Copyright 2015 Alan Drees
+ *
+ * Purpose:
+ *  Implements options structure for the stealthascope-launchpad object
+ *
+ * Dependencies:
+ */
+
 var Launchpad = Launchpad || {};
 
 Launchpad.options = { 'devices'      : 1,
