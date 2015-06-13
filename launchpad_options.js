ef03e1e6fc554c97f42a5b851b4c2d2f12853f15
Alan Drees <alandrees@theselves.com>
2015-06-13 05:24:05 -0400

    added a 'shared_navigation' property, changed the structure of the layout order property, started deprecating the devices property, added channels property

--- launchpad_options.js
+++ launchpad_options.js
@@ -9,9 +9,10 @@
 
 var Launchpad = Launchpad || {};
 
-Launchpad.options = { 'devices'      : 1,
-		      'tracks'       : 8,
-		      'scenes'       : 8,
-		      'startupmode'  : true,
-		      'layout_order' :[{'anchor'   : 1,
-					'position' : 'below'}]};
+Launchpad.options = {'devices'           : 1,
+		     'tracks'            : 8,
+		     'channels'          : 8,
+		     'scenes'            : 8,
+		     'startupmode'       : true,
+		     'shared_navigation' : true,
+		     'layout_order'      : [[[1,1]]]};
