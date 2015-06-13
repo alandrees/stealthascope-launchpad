a2a5d0162ab1f6bf049b4968ffdc21ea9e95b99f
Alan Drees <alandrees@theselves.com>
2015-06-13 08:06:12 -0400

    added updated the onMidi callback-like method to handle merging midi data from multiple sources

--- launchpad_controller_object.js
+++ launchpad_controller_object.js
@@ -502,27 +502,81 @@ Launchpad.LaunchpadController.prototype.drawStealthascopeLogo = function()
 }
 
 
+/**\fn Launchpad.LaunchpadController.prototype.onMidi
  *
+ * Callback function to be called when midi input is detected
  *
+ * @param status (integer) status byte (type/channel) of incoming message
+ * @param data1 (integer) data1 byte of incoming message
+ * @param data2 (integer) data2 byte of incoming message
+ * @param grid_x (integer) x-index of hardware component
+ * @param grid_y (integer) y-index of hardware component
  *
  * @returns None
  */
 
+Launchpad.LaunchpadController.prototype.onMidi = function(status, data1, data2, grid_x, grid_y)
 {
+    if (MIDIChannel(status) != 0) return;
 
+    if (isChannelController(status))
     {
+	var isPressed = data2 > 0;
+
+	switch(data1)
 	{
+	case Launchpad.TopButton.SESSION:
+	    if (isPressed)
 	    {
+		if(this.activePage.name !== "grid"){
+		    this.setActivePage(this.gridPage);
+		}
+		else
+		{
+		    this.gridPage.setTempMode(Launchpad.TEMPMODE.TRACK);
+		}
+	    }
+	    else
+	    {
+		this.gridPage.setTempMode(Launchpad.TEMPMODE.OFF);
 	    }
 
+	    break;
 
+	    case Launchpad.TopButton.USER1:
+	    if (isPressed)
+	    {
+		this.gridPage.setTempMode(Launchpad.TEMPMODE.STOP);
+	    }
+	    else
+	    {
+		this.gridPage.setTempMode(Launchpad.TEMPMODE.OFF);
+	    }
 
+	    break;
 
+	    case Launchpad.TopButton.USER2:
+	    break;
 
+	    case Launchpad.TopButton.MIXER:
+	    this.activePage.onMixerButton(isPressed);
+	    break;
 
+	    case Launchpad.TopButton.CURSOR_LEFT:
+	    this.activePage.onLeft(isPressed);
+	    break;
 
+	    case Launchpad.TopButton.CURSOR_RIGHT:
+	    this.activePage.onRight(isPressed);
+	    break;
 
+	    case Launchpad.TopButton.CURSOR_UP:
+	    this.activePage.onUp(isPressed);
+	    break;
 
+	    case Launchpad.TopButton.CURSOR_DOWN:
+	    this.activePage.onDown(isPressed);
+	    break;
 	}
     }
 }
