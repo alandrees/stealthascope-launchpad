6808f06c6a90b05b6b94df71ec509e46f003a800
Alan Drees <alandrees@theselves.com>
2015-06-13 05:24:40 -0400

    added explanation for the layout order property

--- launchpad_options.js
+++ launchpad_options.js
@@ -9,6 +9,30 @@
 
 var Launchpad = Launchpad || {};
 
+/*
+The layout order option describes the physical layout of the novation launchpad units.
+
+Each of the array pairs are a mapping of input/outputs in the format of [in,out].  This specifies the location for the i/o in the grid.
+
+If you only have one unit, the layout_order parameter will only have the [1,1] unit specified.
+
+Some examples:
+
+single:
+'layout_order'      : [[[1,1]]]
+
+one below:
+'layout_order'      : [[[1,1],[2,2]]]
+
+one right:
+'layout_order'      : [[[1,1]],
+		       [[2,2]]]
+
+group of 4:
+'layout_order'      : [[[1,1],[2,2]],
+		       [[3,3],[4,4]]]
+*/
+
 Launchpad.options = {'devices'           : 1,
 		     'tracks'            : 8,
 		     'channels'          : 8,
