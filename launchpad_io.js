7c1e733c104eff9e76eab151fdb771291c7f10c5
Alan Drees <alandrees@theselves.com>
2015-06-13 06:12:57 -0400

    added file documentation header to launchpad_io.js

---
+++ launchpad_io.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright 2015 Alan Drees
+ *
+ * Purpose:
+ *  Implementation of the LaunchpadIO object for handling the
+ *  interfacing with the launchpads themselves
+ *
+ * Dependencies:
+ */
